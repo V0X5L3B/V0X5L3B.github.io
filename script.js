100d24f122ec0e19183a69129ff395616c3adb09
V0X5L3B <v0x5l3b@users.noreply.github.com>
2026-08-03 21:36:37 -0700

    tagline: coder // developer

--- script.js
+++ script.js
@@ -596,7 +596,7 @@ function initTextScramble() {
         const tagObserver = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    scramble(taglineEl, 'developer // engineer', 800);
+                    scramble(taglineEl, 'coder // developer', 800);
                     tagObserver.unobserve(entry.target);
                 }
             });
